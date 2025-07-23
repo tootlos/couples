2d6cdfb8cd3f3ecb464ff8eebd7946585abea5e5
Tootlos <130791203+tootlos@users.noreply.github.com>
2025-07-23 18:46:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,56 @@
-const inputElement = document.querySelector('.board__input'); 
-const startButton = document.querySelector('.board__button'); 
-const mainElement = document.querySelector('.main');
+document.addEventListener('DOMContentLoaded', function() {
 
-function createBoard(columns, count) {
-  const template = document.getElementById('gameTableTemplate');
-  const templateContent = template.content.cloneNode(true);
-  const tableElement = templateContent.querySelector('.table');
+  const inputElement = document.querySelector('.board__input'); 
+  const startButton = document.querySelector('.board__button'); 
+  const mainElement = document.querySelector('.main');
 
+  function createBoard(columns, count) {
+    const template = document.getElementById('gameTableTemplate');
+    const templateContent = template.content.cloneNode(true);
+    const tableElement = templateContent.querySelector('.table');
 
-  while (tableElement.firstChild) {
-    tableElement.removeChild(tableElement.firstChild);
-  }
-
-  for (let i = 0; i < count; i++) {
-    const newCard = createCard();
-    tableElement.appendChild(newCard);
-  }
   
-  tableElement.style.display = 'grid';
-  tableElement.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
-  tableElement.style.gap = '10px'; 
+    while (tableElement.firstChild) {
+      tableElement.removeChild(tableElement.firstChild);
+    }
+
+    for (let i = 0; i < count; i++) {
+      const newCard = createCard();
+      tableElement.appendChild(newCard);
+    }
+    
+    tableElement.style.display = 'grid';
+    tableElement.style.gridTemplateColumns = `repeat(${columns}, 1fr)`;
+    tableElement.style.gap = '10px'; 
+    
   
+    while (mainElement.firstChild) {
+      mainElement.removeChild(mainElement.firstChild);
+    }
+    mainElement.appendChild(templateContent);
 
-  while (mainElement.firstChild) {
-    mainElement.removeChild(mainElement.firstChild);
+    const restartButton = document.querySelector('.table__button');
+    restartButton.addEventListener('click', () => {
+      location.reload(); 
+    });
   }
-  mainElement.appendChild(templateContent);
 
-  const restartButton = document.querySelector('.table__button');
-  restartButton.addEventListener('click', () => {
-    location.reload(); 
+  startButton.addEventListener('click', () => {
+    const inputNumber = parseInt(inputElement.value, 10);
+    
+    if (inputNumber >= 2 && inputNumber <= 6 && inputNumber % 2 === 0) {
+      const count = inputNumber * inputNumber;
+      createBoard(inputNumber, count);
+    } else {
+      alert('Введите чётное число от 2 до 6!');
+      inputElement.value = "4";
+    }
   });
-}
 
-startButton.addEventListener('click', () => {
-  const inputNumber = parseInt(inputElement.value, 10);
-  
-  if (inputNumber >= 2 && inputNumber <= 6 && inputNumber % 2 === 0) {
-    const count = inputNumber * inputNumber;
-    createBoard(inputNumber, count);
-  } else {
-    alert('Введите чётное число от 2 до 6!');
-    inputElement.value = "4";
+  function createCard() {
+    const cardTemplate = document.getElementById('cardTemplate');
+    const templateContent = cardTemplate.content.cloneNode(true);
+    const cardElement = templateContent.querySelector('.card');
+    return cardElement;
   }
 });
-
-function createCard() {
-  const cardTemplate = document.getElementById('cardTemplate');
-  const templateContent = cardTemplate.content.cloneNode(true);
-  const cardElement = templateContent.querySelector('.card');
-
-  return cardElement;
-}
